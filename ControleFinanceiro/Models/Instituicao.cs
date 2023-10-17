cc7b2b2a6458445cd253de1672f34076425e4d6a
2022101380 <2022101380@LAB2-017>
2023-10-17 20:54:49 -0300

    Adicionar arquivos de projeto.

---
+++ ControleFinanceiro/Models/Instituicao.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace ControleFinanceiro.Models
+{
+    public class Instituicao
+    {
+        [DisplayName("Código")]
+        public long? InstituicaoId { get; set; }
+        public string Nome { get; set; }
+
+        [DisplayName("Endereço")]
+        public string Endereco { get; set; }
+    }
+}
