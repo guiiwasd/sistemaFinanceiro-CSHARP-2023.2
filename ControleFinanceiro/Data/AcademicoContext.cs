f2ac94a18230a8d532408a42cb75cdc35c4b2535
Guilherme Cunha <guidpccunha@gmail.com>
2023-11-07 19:59:44 -0300

    Atualização medo da internet

---
+++ ControleFinanceiro/Data/AcademicoContext.cs
@@ -0,0 +1,12 @@
+﻿using ControleFinanceiro.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControleFinanceiro.Data
+{
+    public class AcademicoContext : DbContext
+    {
+        public AcademicoContext(DbContextOptions<AcademicoContext> options) : base(options) { }
+
+        public DbSet<Instituicao> Instituicoes { get; set; }
+    }
+}
